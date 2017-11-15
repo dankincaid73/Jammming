6884e8198b285900281c4a81646c37089a9945ef
Dan Kincaid <dankincaid@Dans-MacBook-Pro.local>
2017-11-14 17:18:47 -0800

    Built Out All Components and Added Spotify API Functionality

--- src/Components/PlayList/PlayList.js
+++ src/Components/PlayList/PlayList.js
@@ -3,12 +3,23 @@ import './css/PlayList.css';
 import { TrackList } from '../TrackList/TrackList';
 
 export class PlayList extends Component {
+  constructor(props) {
+    super(props);
+    this.handleNameChange = this.handleNameChange.bind(this);
+  }
+
+  handleNameChange(event) {
+    this.props.onNameChange(event.target.value);
+  }
+
   render() {
     return (
       <div className="Playlist">
-        <input value="New Playlist"/>
-          <TrackList />
-        <a className="Playlist-save">SAVE TO SPOTIFY</a>
+        <input defaultValue={this.props.name} onChange={this.handleNameChange} />
+        <TrackList tracks={this.props.tracks} onRemove={this.props.onRemove} />
+        <a className="Playlist-save" onClick={this.props.onSave}>
+          SAVE TO SPOTIFY
+        </a>
       </div>
     );
   }
